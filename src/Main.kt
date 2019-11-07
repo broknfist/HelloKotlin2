66d66e28516ee0e049b142c10fd330de17817a3c
Zsolt Nyemetz <41538231+broknfist@users.noreply.github.com>
2019-11-07 16:09:57 +0100

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,59 @@
+var name1: String? = "Gyuri"
+var greeting: String? = "Hello"
+val name_konst = "Konstans Feri"
+
+fun main() {
+    greetingAll()
+
+    println(helloWorld())
+    sayHello()
+    println(megValami())
+
+    sayWhatIWant("Kisregény következik")
+    sayWhatIWant("Mi az email?")
+}
+
+fun sayWhatIWant(itemToSay:String){
+    println(itemToSay)
+}
+
+fun helloWorld(): String{
+    return "Hello World!"
+}
+
+fun sayHello(){
+    println("Azt mondom hello")
+}
+
+fun megValami() = "Még valami"
+
+fun greetingAll(): Unit{
+    println("$greeting"+" "+"$name1"+"!")
+    name1 = "Kati"
+    greeting = "Szia"
+    println("$greeting"+" "+"$name1"+"!")
+    name1 = null
+    greeting = null
+    println("$greeting"+" "+"$name1"+"!"+" csupa null")
+    greeting = "Szevasz"
+    name1 = "Dónát"
+    println("$greeting"+" "+"$name1"+"!")
+    greeting = "Üdv "
+
+    when (greeting) {
+        null -> greeting = "Hali"
+        else -> greeting = "Jó napot"
+    }
+
+    if (name1 != null) {
+        println("$greeting "+"$name1"+" és "+"$name_konst")
+    } else {
+        println("Szevasz $name_konst!")
+    }
+
+    val newGreeting = if(greeting != null) "Szevasz" else "Halihó"
+    val newGreeting2 = when (greeting) {
+        null -> "Hi"
+        else -> greeting
+    }
+}
